57989dc36630ab72c8afad660a3e76769ca63af1
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2020-10-11 11:48:55 +0200

    First Commit
    Inicializacion del proyecto.
    - Slide Peliculas nuevas
    - Slide Cartelera
    - Slide Peliculas Populares
    - Modal Detalles Peliculas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://api.themoviedb.org/3',
+  apiKey: 'c14ca47f4dbd54f1315fbe912dd4b7d8',
+  imgPath:'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'http://api.themoviedb.org/3',
+  apiKey: 'c14ca47f4dbd54f1315fbe912dd4b7d8',
+  imgPath:'https://image.tmdb.org/t/p'
 };
 
 /*
