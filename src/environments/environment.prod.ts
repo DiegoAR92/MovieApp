c02df333ae5e6a784cf89120f9fcdb1d7782f47b
Diego Alvarez Ruiz <diegoalvarezruiz1@gmail.com>
2020-10-11 12:10:41 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'http://api.themoviedb.org/3',
-  apiKey: 'c14ca47f4dbd54f1315fbe912dd4b7d8',
+  apiKey: '',
   imgPath:'https://image.tmdb.org/t/p'
 };
